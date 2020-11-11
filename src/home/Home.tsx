a5b87f8613c94d45ab383b6ae04835cbf294c1f8
Woody34 <woodruff1606@gmail.com>
2020-11-11 15:55:37 -0600

    init commit

---
+++ src/home/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+// import { useSelector } from 'react-redux';
+// import { RootState } from './redux';
+// import { store } from './redux'
+// import { decrement, increment, reset } from './redux/count/actions';
+
+const Home: React.FC = () => {
+    // const { count } = useSelector((state: RootState) => state.count);
+  
+    // const incrementCount = () => store.dispatch(increment())
+    // const decrementCount = () => store.dispatch(decrement())
+    // const resetCount = () => store.dispatch(reset())
+  
+    return (
+    //   <div className="App">
+    //     <h1>Counter</h1>
+    //     <p>{count}</p>
+    //     <button onClick={incrementCount}>Increment</button>
+    //     <button onClick={decrementCount}>Decrement</button>
+    //     <button onClick={resetCount}>Reset</button>
+    //   </div>
+    );
+  }
+  
+  export default Home;
