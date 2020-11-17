e32741f4d5f522e5f3ed2d511e9a7ea92a87f272
Woody34 <woodruff1606@gmail.com>
2020-11-17 02:27:18 -0600

    replace ts-node with esm-ts-node to support es modules

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,4 +1,3 @@
-/// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -15,7 +14,7 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+export default (_on: unknown, _config: unknown): void => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
