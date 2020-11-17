030c200088a23b0d45e5762284722ce49ee1fadd
Stephen <stephen@vindevcorp.com>
2020-11-16 22:30:04 -0600

    paralax header

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { authRoutes } from '../routes';
+import { useHomeStyles } from './Home.styles';
+
+function ParalaxHeader() {
+  const classes = useHomeStyles();
+  const [offset, setOffset] = useState(0);
+
+  const parallaxShift = () => {
+    setOffset(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', parallaxShift);
+    return () => {
+      window.removeEventListener('scroll', parallaxShift);
+    };
+  });
+
+  return (
+    <div className={classes.App}>
+      <header
+        className={classes.headerBackground}
+        style={{ backgroundPositionY: offset }}>
+        <div className={classes.typography}></div>
+        <div
+          className={classes.techStack}
+          style={{ backgroundPositionY: offset }}></div>
+      </header>
+    </div>
+  );
+}
 
 const Home: React.FC = () => {
   return (
     <>
+      <ParalaxHeader></ParalaxHeader>
       <Link to={authRoutes.podcast} data-cy={authRoutes.podcast}>
         Podcast
       </Link>
