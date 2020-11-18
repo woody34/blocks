176882728132015578b9e203ba38192b2f86f258
Vincent <72934331+thecodingwaffle@users.noreply.github.com>
2020-11-18 09:44:29 -0600

    Feature/sv paralax header rebase (#9)
    * paralax header
    
    * navbar refactor
    
    * nav bar style refactor
    
    * adding favicon,title,and type error fix
    
    * linting auto fix did this
    
    Co-authored-by: Stephen <stephen@vindevcorp.com>

---
+++ src/home/Home.styles.ts
@@ -0,0 +1,71 @@
+import { createUseStyles } from 'react-jss';
+
+export const useHomeStyles = createUseStyles({
+  App: {
+    fontFamily: 'sans-serif',
+    textAlign: 'center',
+    height: '500rem',
+    overflow: 'hidden',
+  },
+
+  headerBackground: {
+    background: 'linear-gradient(#ffffff, #DBDBDC)',
+    backgroundSize: '46rem',
+    height: '50rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    overflow: 'hidden',
+  },
+
+  infoContainer: {
+    bottom: '0px',
+    position: 'absolute',
+    left: '10%',
+    top: '31%',
+    height: 'fit-content',
+    background: 'transparent',
+  },
+
+  typography: {
+    background: 'url("../assets/blocksTypography.svg") no-repeat',
+    backgroundSize: '100%',
+    width: '47vw',
+    maxWidth: '750px',
+    height: '114px',
+    position: 'absolute',
+    margin: 'auto',
+    bottom: '75px',
+    top: '0px',
+    left: '50px',
+  },
+
+  techStack: {
+    background: 'url(../assets/techStacks.png)  no-repeat',
+    backgroundSize: '100%',
+    width: '47vw',
+    maxWidth: '750px',
+    height: 'inherit',
+    margin: 'auto',
+    position: 'absolute',
+    bottom: '0px',
+    right: '0px',
+    top: '24%',
+  },
+
+  linkContainer: {
+    position: 'absolute',
+    top: '0',
+    right: '0',
+    listStyle: 'none',
+    display: 'flex',
+  },
+
+  navLink: {
+    margin: '0.4rem 1rem 0 0',
+    padding: '0 0.3rem',
+    cursor: 'pointer',
+    color: '#b9b9b9',
+  },
+});
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { authRoutes } from '../routes';
+import { useHomeStyles } from './Home.styles';
+import useSticky from '../common/hooks/useSticky';
+import { Navbar } from '../components/Navbar/Navbar';
+
+const ParalaxHeader: React.FC = () => {
+  const classes = useHomeStyles();
+  const [offset, setOffset] = useState(0);
+
+  const parallaxShift = () => {
+    setOffset(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', parallaxShift);
+    return () => {
+      window.removeEventListener('scroll', parallaxShift);
+    };
+  });
+
+  return (
+    <div className={classes.App}>
+      <header
+        className={classes.headerBackground}
+        style={{ backgroundPositionY: offset }}>
+        <div className={classes.typography}></div>
+        <div
+          className={classes.techStack}
+          style={{ backgroundPositionY: offset }}></div>
+        <ul className={classes.linkContainer}>
+          <li className={classes.navLink}>Home</li>
+          <li className={classes.navLink}>Podcast</li>
+          <li className={classes.navLink}>Code</li>
+          <li className={classes.navLink}>Contact us</li>
+        </ul>
+      </header>
+    </div>
+  );
+};
 
 const Home: React.FC = () => {
+  const { sticky } = useSticky();
   return (
     <>
+      <ParalaxHeader></ParalaxHeader>
+      <Navbar sticky={sticky} />
       <Link to={authRoutes.podcast} data-cy={authRoutes.podcast}>
         Podcast
       </Link>
